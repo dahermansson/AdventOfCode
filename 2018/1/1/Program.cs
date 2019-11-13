2c8e4ad1313f6a505075473d1c3a86634d76da17
Daniel <daniel.hermansson@gmail.com>
2019-11-13 23:37:38 +0100

    1a OK

---
+++ 2018/1/1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summa = 0;
+            foreach (var line in File.ReadAllLines("input.txt"))
+                summa += int.Parse(line);
+            Console.WriteLine(summa);
+        }
+    }
+}
