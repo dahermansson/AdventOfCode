f4356700a2169f61271cba42faad2627801481d1
Hermansson <daniel.hermansson@gmail.com>
2019-11-14 15:09:38 +0100

    1b OK

--- 2018/1/1/Program.cs
+++ 2018/1/1/Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+
 namespace _1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var summa = 0;
+            var sum = 0;
+            var lines = new List<string>();
             foreach (var line in File.ReadAllLines("input.txt"))
-                summa += int.Parse(line);
-            Console.WriteLine(summa);
+            {   
+                sum += int.Parse(line);
+                lines.Add(line);
+            }
+            Console.WriteLine(sum);
+            sum = 0;
+            var freq = new List<int>();
+            var cnt = 0;
+            do 
+            {
+                freq.Add(sum);
+                sum += int.Parse(lines[cnt++]);
+                if(cnt == lines.Count())
+                    cnt = 0;
+            } while (freq.IndexOf(sum) < 0);
+            Console.WriteLine(sum);
         }
     }
 }
+
