4b7555886e2b480db9df60fded5043279177120e
Hermansson Daniel <daniel.hermansson@gmail.com>
2019-11-15 17:18:40 +0100

    Dag 2 OK

--- 2018/dag2/star1/Program.cs
+++ 2018/dag2/star1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace star1
 {
@@ -6,7 +8,29 @@ namespace star1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int two = 0;
+            int three = 0;
+            foreach (var line in File.ReadAllLines("input.txt"))
+            {
+                var count = line.GroupBy(t => t).ToDictionary(k =>  k.Key, t => t.Count());
+                bool doneTwo = false;
+                bool doneThree = false;
+                foreach (var c in count.Where(t => t.Value == 2 || t.Value == 3))
+                {
+                    if(!doneTwo && c.Value == 2)
+                    {
+                        two++;
+                        doneTwo = true;
+                    }
+                    if(!doneThree && c.Value == 3)
+                    {
+                        three++;
+                        doneThree = true;
+                    }
+                }
+            }
+
+            Console.WriteLine(two * three);
         }
     }
 }
