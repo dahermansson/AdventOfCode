4faaaf965a04897cfa17969267d14be5233d2f69
Hermansson Daniel <daniel.hermansson@gmail.com>
2019-11-19 17:03:16 +0100

    Dag3 star2 OK

--- 2018/dag3/star2/Program.cs
+++ 2018/dag3/star2/Program.cs
@@ -1,40 +1,44 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
-namespace star1
+namespace star2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var noOwerlaps = new List<int>();
             var fabric = new int[1000,1000];
             foreach (var claim in File.ReadAllLines("input.txt").Select(t => new Claim(t)))
             {
+                bool overlap = false;
                 for (int i = claim.FLeft; i < claim.FLeft + claim.Width; i++)
                 {
                     for (int c = claim.FTop; c < claim.FTop + claim.Height; c++)
                     {
-                        fabric[i,c]++;
+                        if(fabric[i,c] != 0)
+                        {
+                            overlap = true;
+                            if(noOwerlaps.Any(t => t == fabric[i,c]))
+                                noOwerlaps.Remove(fabric[i,c]);
+                        }
+                        fabric[i,c] = claim.ID;
                     }
                 }
-            }
-            int cnt = 0;
-            for (int i = 0; i < 1000; i++)
-            {
-                for (int c = 0; c < 1000; c++)
+                if(!overlap)
                 {
-                    if(fabric[i,c]>1)
-                        cnt++;
+                    noOwerlaps.Add(claim.ID);
                 }
             }
-            Console.WriteLine(cnt);
+            noOwerlaps.ForEach(t => Console.WriteLine(t));
         }
     }
 
     public class Claim
     {
-        public string ID{ get; set; }
+        public int ID { get; set; }
         public int FLeft { get; set; }
         public int FTop { get; set; }
         public int Width { get; set; }
@@ -43,7 +47,7 @@ namespace star1
         public Claim(string claim)
         {
             var split = claim.Split(" ");
-            ID = claim.Substring(0, claim.IndexOf('@'));
+            ID = int.Parse(split[0].Trim('#'));
             FLeft = int.Parse(split[2].Split(',')[0]);
             FTop = int.Parse(split[2].Split(',')[1].Trim(':'));
             Width = int.Parse(split[3].Split('x')[0]);
