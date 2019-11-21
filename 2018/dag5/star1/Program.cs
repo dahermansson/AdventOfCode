8100ce774ec9059ba6d37e7a92126bd43e345584
Hermansson Daniel <daniel.hermansson@gmail.com>
2019-11-21 18:12:30 +0100

    Dag 5 OK

---
+++ 2018/dag5/star1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace star1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            bool thingsRemoved = false;
+            do 
+            {
+                thingsRemoved = false;
+                for (int i = 0; i < input.Length-1; i++)
+                {
+                    if(RemovePair(input[i], input[i+1]))
+                    {
+                        input = input.Remove(i, 2);
+                        thingsRemoved = true;
+                    }
+                }
+            }while (thingsRemoved);
+
+            Console.WriteLine(input.Length);
+        }
+
+        static bool RemovePair(char a, char b)
+        {
+            return Math.Abs(a-b) == 32;
+        }
+    }
+}
---
+++ 2018/dag5/star2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace star2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = string.Empty;
+
+            var res = new List<Tuple<int, int>>();
+            for (int c = 65; c < 91; c++)
+            {
+                input = File.ReadAllText("../star1/input.txt");
+                input = input.Replace(((char)c).ToString(), "").Replace(((char)(c + 32)).ToString(), "");;
+                bool thingsRemoved = false;
+                do 
+                {
+                    thingsRemoved = false;
+                    for (int i = 0; i < input.Length-1; i++)
+                    {
+                        if(RemovePair(input[i], input[i+1]))
+                        {
+                            input = input.Remove(i, 2);
+                            thingsRemoved = true;
+                        }
+                    }
+                }while (thingsRemoved);
+
+                res.Add(new Tuple<int, int>(c, input.Length));
+            }
+            Console.WriteLine(res.Min(t => t.Item2));
+        }
+
+        static bool RemovePair(char a, char b)
+        {
+            return Math.Abs(a-b) == 32;
+        }
+    }
+}
