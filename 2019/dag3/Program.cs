db320820a22d6398d8ceb60e2a95b03f8f889fec
Hermansson <da.hermansson@cgi.com>
2019-12-05 17:39:35 +0100

    dag 3

---
+++ 2019/dag3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Types;
+
+namespace dag3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SqlGeometry s = new SqlGeometry();
+        }
+    }
+}
