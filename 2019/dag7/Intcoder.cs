ba763f46b7b2b160455d292dc156e23a8cf6634c
Hermansson Daniel <daniel.hermansson@gmail.com>
2019-12-11 11:00:37 +0100

    Dag7 star1

---
+++ 2019/dag7/Intcoder.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace AdventOfCode2019
+{
+    public class Intcoder
+    {
+        public int[] IntCode { get; set; }
+        //public int[] inputs {get; set;}
+        //private int inputsIndex = 0;
+
+        public Stack<int> inputs {get; set; }
+        public Intcoder(string intcode)
+        {
+            IntCode = intcode.Split(',').Select(t => int.Parse(t)).ToArray();
+            inputs = new Stack<int>();
+        }
+        public void Init(int position, int value)
+        {
+            IntCode[position] = value;
+        }
+        public int PositionZero { 
+            get
+            {
+                return IntCode[0];
+            }
+        }
+
+        public void Reset(string intcode, bool clearInputOutput)
+        {
+            IntCode = intcode.Split(',').Select(t => int.Parse(t)).ToArray();
+            if(clearInputOutput)
+                inputs.Clear();
+        }
+        public int Exec(int pointer)
+        {
+            int optCode;
+            var instruction = IntCode[pointer].ToString();
+            
+            if(instruction.Length > 1)
+                optCode = int.Parse(instruction.Substring(instruction.Length -2, 2));
+            else
+                optCode = int.Parse(instruction);
+            
+            var param1 = IntCode.Length >= pointer + 1 ? pointer + 1 : 0;
+            var param2 = IntCode.Length >= pointer + 2 ? pointer + 2 : 0;
+            var param3 = IntCode.Length >= pointer + 3 ? pointer + 3 : 0;
+            
+            var param1Mode = 0;
+            var param2Mode = 0;
+            var param3Mode = 0;
+            if(instruction.Length > 2)
+                param1Mode = int.Parse(instruction.Substring(instruction.Length - 3, 1));
+            if(instruction.Length > 3)
+                param2Mode = int.Parse(instruction.Substring(instruction.Length - 4, 1));
+            if(instruction.Length > 4)
+                param3Mode = int.Parse(instruction.Substring(instruction.Length - 5, 1));
+
+            if(optCode == 1)
+                SetParameterValue(param3, GetParamaterValue(param1, param1Mode) + GetParamaterValue(param2, param2Mode));
+            if(optCode == 2)
+                SetParameterValue(param3, GetParamaterValue(param1, param1Mode) * GetParamaterValue(param2, param2Mode));
+            if(optCode == 3)
+            {
+                SetParameterValue(param1, inputs.Pop());
+                return pointer + 2;
+            }
+            if(optCode == 4)
+            {
+                inputs.Push(GetParamaterValue(param1, param1Mode));
+                //Console.WriteLine(inputs.Peek());
+                return pointer + 2;
+            }
+            if(optCode == 5)
+            {
+                if(GetParamaterValue(param1, param1Mode) != 0)
+                    return GetParamaterValue(param2, param2Mode);
+                return pointer + 3;
+            }
+            if(optCode == 6)
+            {
+                if(GetParamaterValue(param1, param1Mode) == 0)
+                    return GetParamaterValue(param2, param2Mode);
+                return pointer + 3;
+            }
+            if(optCode == 7)
+            {
+                if(GetParamaterValue(param1, param1Mode)< GetParamaterValue(param2, param2Mode))
+                    SetParameterValue(param3, 1);
+                else
+                    SetParameterValue(param3, 0);
+            }
+            if(optCode == 8)
+            {
+                if(GetParamaterValue(param1, param1Mode) == GetParamaterValue(param2, param2Mode))
+                    SetParameterValue(param3, 1);
+                else
+                    SetParameterValue(param3, 0);
+            }
+            if(optCode == 99)
+                return -1;
+            return pointer + 4;
+        }
+
+        private void SetParameterValue(int pointer, int value)
+        {
+            IntCode[IntCode[pointer]] = value;   
+        }
+
+        private int GetParamaterValue(int pointer, int mode)
+        {
+            if(mode == 0)
+                return IntCode[IntCode[pointer]];
+            else 
+                return IntCode[pointer];
+        }
+    }
+}
---
+++ 2019/dag7/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using AdventOfCode2019;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+namespace dag7
+{
+    class Program
+    {
+        public static int[] digitArr(int n)
+        {
+            if (n == 0) return new int[1] { 0 };
+
+            var digits = new List<int>();
+
+            for (; n != 0; n /= 10)
+                digits.Add(n % 10);
+
+            var arr = digits.ToArray();
+            Array.Reverse(arr);
+            return arr;
+        }
+
+        static IEnumerable<IEnumerable<T>>GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+        if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(o => !t.Contains(o)),
+                (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        static void Main(string[] args)
+        {
+            Star1();
+            Star2();
+        }
+
+
+        static void Star2()
+        {
+            string input = File.ReadAllText("input.txt");
+            //input = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0";
+            var thrusterSignals = new List<int>();
+            var settings = GetPermutations(new List<int>(){0,1,2,3,4}, 5).Select(t => new List<int>(t));
+            
+            foreach (var parameters in settings)
+            {
+                Intcoder intcoder = new Intcoder(input);
+                intcoder.inputs.Push(0);
+                for (int i = 0; i < 5; i++)
+                {
+                    intcoder.Reset(input, false);
+                    intcoder.inputs.Push(parameters[i]);
+                    var pointer = 0;
+                    do
+                        pointer = intcoder.Exec(pointer);
+                    while (pointer != -1);
+                }
+                //Console.WriteLine(intcoder.inputs.Peek());
+                thrusterSignals.Add(intcoder.inputs.Peek());
+            }
+        
+            Console.WriteLine($"Star 1: {thrusterSignals.Max()}");
+        }
+
+        static void Star1()
+        {
+            string input = File.ReadAllText("input.txt");
+            //input = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0";
+            var thrusterSignals = new List<int>();
+            var settings = GetPermutations(new List<int>(){0,1,2,3,4}, 5).Select(t => new List<int>(t));
+            
+            foreach (var parameters in settings)
+            {
+                Intcoder intcoder = new Intcoder(input);
+                intcoder.inputs.Push(0);
+                for (int i = 0; i < 5; i++)
+                {
+                    intcoder.Reset(input, false);
+                    intcoder.inputs.Push(parameters[i]);
+                    var pointer = 0;
+                    do
+                        pointer = intcoder.Exec(pointer);
+                    while (pointer != -1);
+                }
+                //Console.WriteLine(intcoder.inputs.Peek());
+                thrusterSignals.Add(intcoder.inputs.Peek());
+            }
+        
+            Console.WriteLine($"Star 1: {thrusterSignals.Max()}");
+        }
+    }
+}
