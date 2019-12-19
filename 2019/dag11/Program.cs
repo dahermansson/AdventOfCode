cd859925485bc7c2e79c2aa33b19547ac62a362f
Daniel <daniel.hermansson@gmail.com>
2019-12-19 21:55:51 +0100

    dag11 star2 ok

--- 2019/dag11/Program.cs
+++ 2019/dag11/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Linq;
 using AdventOfCode2019;
+using System.Drawing;
 
 namespace dag11
 {
@@ -12,45 +14,43 @@ namespace dag11
             Star1();
             Star2();
         }
-
         private static void Star2()
         {
             Intcoder intcoder = new Intcoder(Utils.ReadAllInput("input.txt"), OutputMode.ReturnAndHalt);
             var robot = new Robot();
-            robot.PaintedPanels.Add(new Tuple<int, int>(0,0), new Panel(){CurrentColor = Color.White});
+            robot.PaintedPanels.Add(new Tuple<int, int>(100,100), new Panel(){CurrentColor = Color.White});
             var input = new Queue<System.Numerics.BigInteger>();
-            var nextPosition = new Tuple<int,int>(0,0);
-            var runs = 0;
+            var nextPosition = new Tuple<int,int>(100,100);
             while (!intcoder.Done)
             {
-                runs++;
                 input.Enqueue((int)robot.ColorInPosition(nextPosition));
                 var paintedColor = intcoder.Exec(input) == 0 ? Color.Black : Color.White;
                 var directionTurned = intcoder.Exec() == 0 ? Turn.Left : Turn.Right;
                 nextPosition = robot.GoToNextPosition(paintedColor, directionTurned);
             }
-            Console.WriteLine($"Star 2: {robot.PaintedPanels.Count}");
+            var width = Math.Abs(robot.PaintedPanels.Max(t => t.Key.Item1));
+            var height = Math.Abs(robot.PaintedPanels.Max(t => t.Key.Item2));
+            var bitmap = new Bitmap(height + 1, width + 1);
+            foreach (var panel in robot.PaintedPanels)
+            {
+                bitmap.SetPixel(Math.Abs(panel.Key.Item2), Math.Abs(panel.Key.Item1), panel.Value.CurrentColor == dag11.Color.Black ? System.Drawing.Color.Black : System.Drawing.Color.White);
+                bitmap.Save("dag11Star2.png");     
+            }
         }
-
-
-
         private static void Star1()
         {
             Intcoder intcoder = new Intcoder(Utils.ReadAllInput("input.txt"), OutputMode.ReturnAndHalt);
             var robot = new Robot();
             var input = new Queue<System.Numerics.BigInteger>();
-            var nextPosition = new Tuple<int,int>(0,0);
-            var runs = 0;
+            var nextPosition = new Tuple<int,int>(100,100);
             while (!intcoder.Done)
             {
-                runs++;
                 input.Enqueue((int)robot.ColorInPosition(nextPosition));
                 var paintedColor = intcoder.Exec(input) == 0 ? Color.Black : Color.White;
                 var directionTurned = intcoder.Exec() == 0 ? Turn.Left : Turn.Right;
                 nextPosition = robot.GoToNextPosition(paintedColor, directionTurned);
             }
-
-            Console.WriteLine($"Star 1: {robot.PaintedPanels.Count}");
+            Console.WriteLine($"Star 1: {robot.PaintedPanels.Count - 1}");
         }
     }
 
@@ -59,7 +59,7 @@ namespace dag11
         public Robot()
         {
             CurrentDirection = Direction.Up;
-            CurrentPosition = new Tuple<int, int>(0,0);
+            CurrentPosition = new Tuple<int, int>(100,100);
             PaintedPanels = new Dictionary<Tuple<int, int>, Panel>();
         }
         public Tuple<int, int> CurrentPosition { get; set; }
