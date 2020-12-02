f0e0f297d763735757e7d007bd40c1ddc20c7bd0
Hermansson Daniel <daniel.hermansson@gmail.com>
2020-12-02 17:12:41 +0100

    Dag 2

---
+++ 2020/Dag2.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+namespace AoC2020
+{
+    public class Dag2 : IDag
+    {
+        private static string[] InputLines = InputReader.GetInputLines("dag2.txt");
+        public int Star1()
+        {
+            return InputLines.Select(t => new PolicyPassword(t.Split(' '))).Count(t => t.IsValidToSledRentalPlaceDownTheStreet());
+        }
+
+        public int Star2()
+        {
+            return InputLines.Select(t => new PolicyPassword(t.Split(' '))).Count(t => t.IsValidToOfficialTobogganCorporate());
+        }
+
+        private class PolicyPassword
+        {
+            public PolicyPassword(string[] linepParts)
+            {
+                Least = int.Parse(linepParts[0].Split('-')[0]);
+                Most = int.Parse(linepParts[0].Split('-')[1]);
+                Char = linepParts[1][0];
+                Password = linepParts[2];
+            }
+
+            public int Least { get; private set; }
+            public int Most { get; private set; }
+            public char Char { get; private set; }
+            public string Password { get; private set; }
+
+            public bool IsValidToSledRentalPlaceDownTheStreet()
+            {
+                return Enumerable.Range(Least, Most - Least +1).Contains(Password.Count(c => c == Char));
+            }
+
+            public bool IsValidToOfficialTobogganCorporate()
+            {
+                return (Password[Least-1] == Char && Password[Most-1] != Char) ||
+                    (Password[Least-1] != Char && Password[Most-1] == Char);
+            }
+        }
+    }
+}
--- 2020/Program.cs
+++ 2020/Program.cs
@@ -5,11 +5,12 @@ namespace AoC2020
 {
     class Program
     {
-        private static SortedList<int, IDag> Dagar = new SortedList<int, IDag>();
-        private static readonly int DagToRun = 1;
+        private static Dictionary<int, IDag> Dagar = new Dictionary<int, IDag>();
+        private static readonly int DagToRun = 2;
         static void Main(string[] args)
         {
             Dagar.Add(1, new Dag1());
+            Dagar.Add(2, new Dag2());
 
             Console.WriteLine($"Star 1: {Dagar[DagToRun].Star1()}");
             Console.WriteLine($"Star 2: {Dagar[DagToRun].Star2()}");
