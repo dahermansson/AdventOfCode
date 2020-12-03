093a8c37524235fe73199d9b3c0a4176479abe2e
Hermansson Daniel <daniel.hermansson@gmail.com>
2020-12-03 17:37:40 +0100

    Dag 3 done

--- 2020/Dag3.cs
+++ 2020/Dag3.cs
@@ -1,31 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AoC2020
 {
     public class Dag3 : IDag
     {
         private static string[][] InputLines = InputReader.GetInputLinesMatrix("dag3.txt");
-        public int Star1()
-        {
-            return TobogganRun((1, 3));
-        }
-
+        public int Star1() => TobogganRun((1, 3));
         public int Star2()
         {
-            return TobogganRun((1,1)) * TobogganRun((1,3)) * TobogganRun((1,5)) * TobogganRun((1,7)) * TobogganRun((2,1));
+            var slopes = new [] { (1,1),(1,3),(1,5),(1,7),(2,1) };
+            return DoTobogganRuns(slopes).Aggregate((prod, next) => prod * next);
         }
+
+        private IEnumerable<int> DoTobogganRuns((int x, int y)[] slopes)
+        {
+            foreach (var slope in slopes)
+                yield return TobogganRun(slope);    
+        }   
 
         private int TobogganRun((int x, int y) slope)
         {
-            //var slope = (x: 1,y: 3);
             var position = (x: 0, y: 0);
             var treeCount = 0;
             while (position.x < InputLines.Length)
             {
-                if(InputLines[position.x][position.y] == "#")
-                    treeCount++;
+                if(InputLines[position.x][position.y % InputLines[0].Length] == "#")
+                   treeCount++;
                 position.x += slope.x;
                 position.y += slope.y;
-                if(position.y >= InputLines[0].Length)
-                    position.y -= InputLines[0].Length;
             }
             return treeCount;
         }
