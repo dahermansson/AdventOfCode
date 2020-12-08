c424aead308b3d6bfb5f77d154bbea85d6a61d4a
Daniel <daniel.hermansson@gmail.com>
2020-12-08 19:59:47 +0100

    dag 3 2015

--- 2015/Program.cs
+++ 2015/Program.cs
@@ -6,11 +6,12 @@ namespace AoC2015
     class Program
     {
         private static Dictionary<int, IDag> Dagar = new Dictionary<int, IDag>();
-        private static readonly int DagToRun = 2;
+        private static readonly int DagToRun = 3;
         static void Main(string[] args)
         {
             Dagar.Add(1, new Dag1());
             Dagar.Add(2, new Dag2());
+            Dagar.Add(3, new Dag3());
 
             Console.WriteLine($"Star 1: {Dagar[DagToRun].Star1()}");
             Console.WriteLine($"Star 2: {Dagar[DagToRun].Star2()}");
