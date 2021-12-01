b9817656eaa9eba4708b77ada7cb160fc2e77dcf
Daniel <daniel.hermansson@gmail.com>
2021-12-01 21:52:37 +0100

    Day 1

--- 2021/Day1.cs
+++ 2021/Day1.cs
@@ -9,15 +9,16 @@ namespace AoC2021
     public class Day1 : IDay
     {
         public string Output => throw new NotImplementedException();
+        public int[] Input = InputReader.GetInputLines<int>("1.txt");
 
-        public int Star1()
-        {
-            return 1;
-        }
+        private int CountGreaterThenPrevius(int[] integers) => integers.Select((v, i) => new {Index = i, Value = v }).Skip(1).Count(t => t.Value > integers[t.Index-1]);
+
+        public int Star1() => CountGreaterThenPrevius(Input);
 
         public int Star2()
         {
-            return 2;
+            var slidingWindows = Input.SkipLast(2).Select((v, i) => new {Value = Input.Skip(i).Take(3).Sum()}).Select(t => t.Value).ToArray();
+            return CountGreaterThenPrevius(slidingWindows);
         }
     }
 }
