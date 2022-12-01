89cbb095e1c40773640d41df23a0b094d41618fb
DaHermansson <daniel.hermansson@gmail.com>
2022-12-01 19:38:59 +0100

    Day1 Onelinelinq

--- 2022/Day1.cs
+++ 2022/Day1.cs
@@ -1,8 +1,13 @@
 public class Day1 : IDay
 {
     public string Output => throw new NotImplementedException();
-    public int Star1() => SumCalories().Max();
-    public int Star2() => SumCalories().Order().TakeLast(3).Sum();
+    //public int Star1() => SumCalories().Max();
+    public int Star1() => GetMaxSumCalories();
+    //public int Star2() => SumCalories().Order().TakeLast(3).Sum();
+    public int Star2() => GetSumTop3SumCalories();
+
+    private int GetMaxSumCalories() => InputReader.GetInput("day1.txt").Split("\r\n\r\n").Select(p => p.Split("\r\n").Select(int.Parse).Sum()).Max();
+    private int GetSumTop3SumCalories() => InputReader.GetInput("day1.txt").Split("\r\n\r\n").Select(p => p.Split("\r\n").Select(int.Parse).Sum()).Order().TakeLast(3).Sum();
     private IEnumerable<int> SumCalories() => InputReader.GetInputLines("day1.txt").Aggregate(new List<int> { 0 }, (l, n) =>
     {
         if (int.TryParse(n, out int x))
