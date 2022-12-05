4f413db6d368abb254596aaf7f09b34971665092
DaHermansson <daniel.hermansson@gmail.com>
2022-12-05 22:11:54 +0100

    Read all input Day5

--- 2022/Day5.cs
+++ 2022/Day5.cs
@@ -1,27 +1,35 @@
 public class Day5 : IDay
 {
     public string Output => _output;
-    private string _output;
-    Dictionary<int, Stack<string>> Stacks;
+    private string _output = string.Empty;
+    Dictionary<int, Stack<char>> Stacks;
+    private const int STACKS = 9;
+
     public Day5()
     {
         Stacks = InitStacks();
-        _output = string.Empty;
     }
+    
+    private Dictionary<int, Stack<char>> InitStacks()
+    {
+        var stacks = new Dictionary<int, Stack<char>>();
+        for (int i = 1; i <= STACKS; i++)
+            stacks.Add(i, new Stack<char>());
 
-    //Don't do this!!! Read input from file :(
-    private Dictionary<int, Stack<string>> InitStacks() =>
-        new Dictionary<int, Stack<string>>{
-            {1, new Stack<string>(new string[]{"W", "D", "G", "B", "H", "R", "V"})},
-            {2, new Stack<string>(new string[]{"J", "N", "G", "C", "R", "F"})},
-            {3, new Stack<string>(new string[]{"L", "S", "F","H","D","N","J"})},
-            {4, new Stack<string>(new string[]{"J", "D", "S", "V"})},
-            {5, new Stack<string>(new string[]{"S","H","D","R","Q","W","N","V"})},
-            {6, new Stack<string>(new string[]{"P", "G", "H", "C", "M"})},
-            {7, new Stack<string>(new string[]{"F", "J", "B", "G", "L", "Z","H", "C"})},
-            {8, new Stack<string>(new string[]{"S", "J", "R"})},
-            {9, new Stack<string>(new string[]{"L", "G", "S", "R", "B", "N","V", "M"})}
-        };
+        var stackLines = InputReader.GetInputLines("Day5.txt").Take(8).Reverse();
+        foreach (var line in stackLines)
+        {
+            var readIndex = 0;
+            for (int i = 1; i <= STACKS; i++)
+            {
+                var value = line.Substring(readIndex, i == STACKS ? 3 : 4)[1];
+                if(char.IsLetter(value))
+                    stacks[i].Push(value);
+                readIndex+=4;
+            }
+        }
+        return stacks;
+    }
 
     public int Star1()
     {
@@ -37,7 +45,7 @@ public class Day5 : IDay
         Stacks = InitStacks();
         foreach (var instruction in InputReader.GetInputLines("Day5.txt").Skip(10).Select(t => (Move: int.Parse(t.Split(" ")[1]), Fr: int.Parse(t.Split(" ")[3]), To: int.Parse(t.Split(" ")[5]))))
         {
-            var reverseChunkStack = new Stack<string>();
+            var reverseChunkStack = new Stack<char>();
             for (int i = 0; i < instruction.Move; i++)
                 reverseChunkStack.Push(Stacks[instruction.Fr].Pop());
             for (int i = 0; i < instruction.Move; i++)
