1b6cf7716c37e6190fe76d5c54d26daae48506d9
Daniel Hermansson <daniel.hermanssono@trafikverket.se>
2022-12-21 16:32:03 +0100

    Day18 1

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -17,10 +17,11 @@ Days.Add(13, new Day13());
 Days.Add(14, new Day14());
 Days.Add(15, new Day15());
 Days.Add(16, new Day16());
+Days.Add(18, new Day18());
 Days.Add(20, new Day20());
 Days.Add(21, new Day21());
 
-int dayToRun = Days.Last().Key;
+int dayToRun = 18;//Days.Last().Key;
 var star1 = Days[dayToRun].Star1();
 var output = star1 == -1 ? Days[dayToRun].Output: star1.ToString(); 
 Console.WriteLine($"Star 1: { output}");
